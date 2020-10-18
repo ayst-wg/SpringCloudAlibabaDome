d7c46f443fba71fdefa9ac0049f66df812825294
Administrator <wjiwg.liu.gg@126.com>
2020-10-18 23:09:00 +0800

    update

---
+++ Cloud-Alibaba-Redis6379/src/main/java/com/wanggang/alibaba/Redis6379Application.java
@@ -0,0 +1,18 @@
+package com.wanggang.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName : RedisApplication
+ * @Description : RedisApplication
+ * @Author : Administrator
+ * @Date: 2020-10-18 23:01
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class Redis6379Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Redis6379Application.class,args);
+    }
+}
---
+++ Cloud-Alibaba-Redis6379/src/main/java/com/wanggang/alibaba/contorller/RedisContorller.java
@@ -0,0 +1,12 @@
+package com.wanggang.alibaba.contorller;
+
+/**
+ * @ClassName : RedisContorller
+ * @Description : RedisContorller
+ * @Author : Administrator
+ * @Date: 2020-10-18 23:00
+ * @Version 1.0
+ **/
+public class RedisContorller {
+
+}
